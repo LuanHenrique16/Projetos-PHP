6966e2446ef1e1d8ecfd6e31a4e71ab16f64eaf3
UNASP-HT - Pesquisa <pesquisa@unasp-ht.edu.br>
2021-02-22 12:03:33 -0300

    regcity

--- reg-city.php
+++ reg-city.php
@@ -1,5 +1,4 @@
 <?php
-include 'check.php'
 include 'connect.php';
 if(isset($_POST['sub'])){
     $namecity=$_POST['name city'];
--- reg.php
+++ reg.php
@@ -1,5 +1,4 @@
 <?php
-include 'check.php'
 include 'connect.php';
 if(isset($_POST['sub'])){
     $t=$_POST['text'];
